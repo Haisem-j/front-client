cfc6f7bf0eae1ed2cdb8d7f26540a822689fe4d1
Haisem Jemal <haisem.jemal@gmail.com>
2021-11-24 14:29:15 -0500

    wrapped new recipe in form control

--- src/pages/DashBoard/Recipes/NewRecipe/index.jsx
+++ src/pages/DashBoard/Recipes/NewRecipe/index.jsx
@@ -1,17 +1,31 @@
 import React, { useState } from 'react'
 import UploadPicture from '../UploadPicture';
-import { Layout, Row, Col, Typography, Divider, Button, Card, Input } from 'antd';
+import { Layout, Row, Col, Typography, Divider, Form, Card, Input, Button } from 'antd';
 
 const { Content } = Layout;
 const { Title, Text } = Typography;
 const { TextArea } = Input
 
+/* eslint-disable no-template-curly-in-string */
+
+const validateMessages = {
+    required: '${label} is required!',
+    types: {
+        email: '${label} is not a valid email!',
+        number: '${label} is not a valid number!',
+    },
+    number: {
+        range: '${label} must be between ${min} and ${max}',
+    },
+};
+/* eslint-enable no-template-curly-in-string */
 
 export default function NewRecipe() {
     const [content, setContent] = useState('')
 
-    const handleSubmit = () => {
-
+    const handleSubmit = (values) => {
+        console.log('HANDLE DAT');
+        console.log(values);
     }
     return (
         <Layout>
@@ -25,24 +39,54 @@ export default function NewRecipe() {
                 <Row>
                     <Col span={12}>
                         <Card>
-                            <Title level={5}>Title <span className="required">*</span></Title>
-                            <Input placeholder="eg., 👋 Cheesy Baked Eggs" />
-                            <Divider />
-                            <Title level={5}>Image</Title>
-                            <Text type="secondary" >Add a header image to appear between the title and contents</Text>
-                            <div className="" style={{ marginBottom: '20px' }} />
-                            <UploadPicture />
-                            <Divider />
-                            <Title level={5}>Contents <span className="required">*</span></Title>
-                            <Text type="secondary" >Add text, links, bullets, and numbered lists. Perfect for a simple description, things the customer should keep in mind or other small notes.</Text>
-                            <div className="" style={{ marginBottom: '20px' }} />
-                            <TextArea showCount maxLength={100} onChange={(e) => { setContent(e.target.value) }} placeholder="Add contents here..." allowClear />
-                            <Divider />
-                            <Title level={5}>Video URL</Title>
-                            <Text type="secondary" >Add a YouTube video, Vimeo link, Instagram post/reel, or TikTok to your resource. Copy and paste the link below.</Text>
-                            <div className="" style={{ marginBottom: '20px' }} />
-                            <Input placeholder="https://..." />
-                            <Divider />
+                            <Form onFinish={handleSubmit} validateMessages={validateMessages}>
+                                <Title level={5}>Title <span className="required">*</span></Title>
+                                <Form.Item
+                                    name="title"
+                                    rules={[
+                                        {
+                                            required: true,
+                                        },
+                                    ]}
+                                >
+                                    <Input placeholder="eg., 👋 Cheesy Baked Eggs" />
+                                </Form.Item>
+                                <Divider />
+
+                                <Title level={5}>Image</Title>
+                                <Text type="secondary" >Add a header image to appear between the title and contents</Text>
+                                <div className="" style={{ marginBottom: '20px' }} />
+                                <Form.Item
+                                    name="upload"
+                                >
+                                    <UploadPicture />
+                                </Form.Item>
+                                <Divider />
+                                <Title level={5}>Contents <span className="required">*</span></Title>
+                                <Text type="secondary" >Add text, links, bullets, and numbered lists. Perfect for a simple description, things the customer should keep in mind or other small notes.</Text>
+                                <div className="" style={{ marginBottom: '20px' }} />
+                                <Form.Item
+                                    name="content"
+                                >
+                                    <TextArea showCount maxLength={100} onChange={(e) => { setContent(e.target.value) }} placeholder="Add contents here..." allowClear />
+                                </Form.Item>
+                                <Divider />
+                                <Title level={5}>Video URL</Title>
+
+                                <Text type="secondary" >Add a YouTube video, Vimeo link, Instagram post/reel, or TikTok to your resource. Copy and paste the link below.</Text>
+                                    <div className="" style={{ marginBottom: '20px' }} />
+                                <Form.Item
+                                    name="videoLink"
+                                >
+                                    <Input placeholder="https://..." />
+                                </Form.Item>
+                                <Divider />
+                                <Form.Item>
+                                    <Button type="primary" htmlType="submit" block>
+                                        Submit
+                                    </Button>
+                                </Form.Item>
+                            </Form>
                         </Card>
                     </Col>
                 </Row>
--- src/pages/DashBoard/Recipes/UploadPicture/index.jsx
+++ src/pages/DashBoard/Recipes/UploadPicture/index.jsx
@@ -1,6 +1,6 @@
+import React, { useState } from 'react';
 import { Upload, message } from 'antd';
 import { LoadingOutlined, PlusOutlined } from '@ant-design/icons';
-import { useState } from 'react';
 
 function getBase64(img, callback) {
     const reader = new FileReader();
