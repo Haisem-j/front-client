67a0d9f7f363ac46d50689468bdd8bc8baafaed8
Haisem Jemal <haisem.jemal@gmail.com>
2021-12-01 11:14:55 -0500

    last update for awhile

--- src/pages/DashBoard/Recipes/NewRecipe/RecipeInstructions.jsx
+++ src/pages/DashBoard/Recipes/NewRecipe/RecipeInstructions.jsx
@@ -2,37 +2,59 @@ import React, { useState } from 'react'
 
 import { Row, Col, Card, Input, Typography, Button, Alert } from 'antd';
 import { MenuOutlined, CloseOutlined, CheckOutlined } from '@ant-design/icons'
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 const { Title } = Typography;
 
+const tempRecipes = [
+    'First oen',
+    'Second one'
+]
 export default function RecipeInstructions() {
-    const [instructions, setInstructions] = useState([])
+    const [instructions, setInstructions] = useState([1])
     const [addInstruction, setAddInstruction] = useState(false);
     const [singleInstruction, setSingleInstruction] = useState('');
     const [error, setError] = useState('')
 
     const renderInstructions = () => {
-        return instructions.map((item) => {
+        return tempRecipes.map((item, index) => {
             return (
-                <Col span={24}>
-                    <Card>
-                        <Row>
-                            <Col>
-                                {item}
-                            </Col>
-                            <Col flex={1} className="instruction-right" style={{ marginTop: 10 }}>
-                                <MenuOutlined style={{ marginRight: 10 }} className="instruction-icon" />
-                                <CloseOutlined className="instruction-icon" style={{ color: 'red' }} />
-                            </Col>
-                        </Row>
-                    </Card>
-                </Col>
+                <Draggable key={`${index}:recipe`} draggableId={`${index}:recipe`} index={index}>
+                    {(provided) => {
+                        <Col span={24} {...provided.droppableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
+                            <Card>
+                                <Row>
+                                    <Col>
+                                        {item}
+                                    </Col>
+                                    <Col flex={1} className="instruction-right" style={{ marginTop: 10 }}>
+                                        <MenuOutlined style={{ marginRight: 10 }} className="instruction-icon" />
+                                        <CloseOutlined className="instruction-icon" style={{ color: 'red' }} />
+                                    </Col>
+                                </Row>
+                            </Card>
+                        </Col>
+                    }}
+                </Draggable>
             )
         })
     }
 
-    const handleSubmit = () =>{
-        if(singleInstruction.length === 0){
+    const renderDrag = () => {
+        return (
+            <DragDropContext>
+                <Droppable droppableId="recipeInstructionsDroppable">
+                    {(provided) => {
+                        <Row {...provided.droppableProps} ref={provided.innerRef}>
+                            {renderInstructions()}
+                        </Row>
+                    }}
+                </Droppable>
+            </DragDropContext>
+        )
+    }
+    const handleSubmit = () => {
+        if (singleInstruction.length === 0) {
             setError(true)
             return null
         }
@@ -46,13 +68,13 @@ export default function RecipeInstructions() {
             <Card>
                 <Row>
                     <Col flex={10}>
-                        <Input placeholder="Add instruction here..." value={singleInstruction} onChange={(e) =>{
+                        <Input placeholder="Add instruction here..." value={singleInstruction} onChange={(e) => {
                             setSingleInstruction(e.target.value);
-                        }}/>
+                        }} />
                     </Col>
                     <Col flex={1} className="instruction-right">
-                        <CheckOutlined style={{ marginRight: 10, color: 'green' }} className="instruction-icon" onClick={handleSubmit}/>
-                        <CloseOutlined className="instruction-icon" style={{ color: 'red' }} onClick={() => setAddInstruction(false)}/>
+                        <CheckOutlined style={{ marginRight: 10, color: 'green' }} className="instruction-icon" onClick={handleSubmit} />
+                        <CloseOutlined className="instruction-icon" style={{ color: 'red' }} onClick={() => setAddInstruction(false)} />
                     </Col>
                 </Row>
             </Card>
@@ -64,9 +86,7 @@ export default function RecipeInstructions() {
             {error && <Alert message="Insturction empty!" type="error" showIcon />}
             {addInstruction && renderAddInstruction()}
             {instructions.length > 0 && <Title level={5} style={{ marginTop: 10 }}>Content</Title>}
-            <Row>
-                {renderInstructions()}
-            </Row>
+            {instructions.length > 0 && renderDrag()}
         </>
     )
 }
